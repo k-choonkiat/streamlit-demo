df6a1d9176d003b540ae0d80506c9953bc64d244
ChoonKiat <choonkiat.dynp@gmail.com>
2021-06-20 00:43:57 +0800

    first prototype

--- main.py
+++ main.py
@@ -6,35 +6,28 @@ import requests
 import json
 
 #importing the data for weather 
-weather_data = requests.get("https://api.data.gov.sg/v1/environment/pm25").json()
-weather_data = pd.DataFrame.from_dict(weather_data)
+pm_data = requests.get("https://api.data.gov.sg/v1/environment/pm25").json()
+region_info = pd.DataFrame.from_dict(pm_data["region_metadata"])
+region_info = pd.concat([region_info.drop(['label_location'],axis=1),region_info['label_location'].apply(pd.Series)],axis=1)
+pm_info = pd.DataFrame.from_dict(pm_data['items'][0])
+pm_info = pd.concat([pm_info.drop(['readings'],axis=1),pm_info['readings'].apply(pd.Series)],axis=1)
+#pre-processing of data
+
 
 st.title('Weather and environment application')
 
-st.write(weather_data)
+st.map(region_info)
 
-if st.checkbox('Show dataframe'):
-    chart_data = pd.DataFrame(
-       np.random.randn(20, 3),
-       columns=['a', 'b', 'c'])
+st.header("The raw data as tables")
+st.subheader("Region data")
+st.write(region_info)
+st.subheader("PM data")
+st.write(pm_info)
 
-    chart_data
 
-
-left_column, right_column = st.beta_columns(2)
-pressed = left_column.button('Press me?')
-if pressed:
-    right_column.write("Woohoo!")
-
-expander = st.beta_expander("FAQ")
-expander.write("Here you could put in some really, really long explanations...")
-
-# Add a placeholder
-latest_iteration = st.empty()
-bar = st.progress(0)
-
-for i in range(100):
-  # Update the progress bar with each iteration.
-  latest_iteration.text(f'Iteration {i+1}')
-  bar.progress(i + 1)
-  time.sleep(0.1)
+expander = st.beta_expander("What is this")
+expander.write("A test project I wanted to create to test out Streamlit :)")
+expander = st.beta_expander("What is my end goal?")
+expander.write("A simple app that will dislpay PSI and also weather data and maybe even with alert systems")
+expander = st.beta_expander("To do:")
+expander.write("add weather data, add UV data")
