f055bbbc0d5c9f654ff40ac14664ffb9f5bca1f8
ChoonKiat <choonkiat.dynp@gmail.com>
2021-06-20 13:54:41 +0800

    added map with elevation features combined region and pm tables

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import time
 import requests
-import json
+import pydeck as pdk
 
 #importing the data for weather 
 pm_data = requests.get("https://api.data.gov.sg/v1/environment/pm25").json()
@@ -11,14 +10,52 @@ region_info = pd.DataFrame.from_dict(pm_data["region_metadata"])
 region_info = pd.concat([region_info.drop(['label_location'],axis=1),region_info['label_location'].apply(pd.Series)],axis=1)
 pm_info = pd.DataFrame.from_dict(pm_data['items'][0])
 pm_info = pd.concat([pm_info.drop(['readings'],axis=1),pm_info['readings'].apply(pd.Series)],axis=1)
-#pre-processing of data
 
+#pre-processing of data
+midpoint = (np.average(region_info['latitude']), np.average(region_info['longitude']))
+pm = pm_info[['west','east','central','south','north']].T.reset_index()
+pm = pm.rename(columns={"index":"region"})
+df = region_info.merge(pm,left_on="name",right_on="region")
+df = df.drop(['region'],axis=1)
 
 st.title('Weather and environment application')
 
-st.map(region_info)
+#st.map(region_info)
+
+st.pydeck_chart(pdk.Deck(
+     map_style='mapbox://styles/mapbox/light-v9',
+     initial_view_state=pdk.ViewState(
+         latitude=midpoint[0],
+         longitude=midpoint[1],
+         zoom=11,
+         pitch=50,
+     ),
+     layers=[
+
+        pdk.Layer(
+             'ScatterplotLayer',
+             data=df,
+             get_position='[longitude, latitude]',
+             get_color='[200, 30, 0, 160]',
+             get_radius=200,
+         ),
+         pdk.Layer(
+            "ColumnLayer",
+            data=df,
+            get_position='[longitude, latitude]',
+            get_elevation='[pm25_one_hourly]',
+            elevation_scale=100,
+            radius=200,
+            get_fill_color='[100,30,100,255]',
+            pickable=True,
+            auto_highlight=True,
+)
+     ],
+ ))
 
 st.header("The raw data as tables")
+st.subheader("Combined table")
+st.write(df)
 st.subheader("Region data")
 st.write(region_info)
 st.subheader("PM data")
